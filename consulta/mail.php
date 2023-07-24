ff530a0a21cff236653a78b8a9bd0f0bab3b3245
astrasol <antony.bz@astratechsolutionssac.com>
2023-07-24 18:01:01 -0500

    envio de correo

--- consulta/mail.php
+++ consulta/mail.php
@@ -6,14 +6,11 @@
     $mensaje = $_POST['mensaje'];
 
     $header = "From: noreply@midominio.com\r\n";
-    $header .= "Reply-To: adminstradoras@astratechsolutionssac.com\r\n";
+    $header = "Reply-To: adminstradoras@astratechsolutionssac.com\r\n";
     
 
     mail($destinatario, $asunto, $mensaje, $header);
 
-    echo "
-    <script>
-    alert('Correo enviado con éxito. Pronto te responderemos!')
-    </script>";
+    
     
 ?>
